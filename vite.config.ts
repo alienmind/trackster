96e75d2510fe9c29a0e8308d26087d888edcae6d
Alien Mind <alienmind@gmail.com>
2026-05-25 22:58:05 +0200

    build: set base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
+  base: '/trackster/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
